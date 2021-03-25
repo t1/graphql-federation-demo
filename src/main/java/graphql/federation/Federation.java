47e50282f4334e9be3b10c9adc9148b89feed644
Rüdiger zu Dohna <snackbox@sinntr.eu>
2021-03-25 08:08:11 +0100

    dynamically find _Entity types

--- src/main/java/graphql/federation/Federation.java
+++ src/main/java/graphql/federation/Federation.java
@@ -10,7 +10,6 @@ import graphql.schema.CoercingParseValueException;
 import graphql.schema.CoercingSerializeException;
 import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.FieldCoordinates;
-import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLCodeRegistry;
 import graphql.schema.GraphQLDirective;
 import graphql.schema.GraphQLFieldDefinition;
@@ -19,11 +18,16 @@ import graphql.schema.GraphQLScalarType;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.GraphQLSchema.Builder;
 import graphql.schema.GraphQLUnionType;
+import io.smallrye.graphql.schema.ScanningContext;
 import io.smallrye.graphql.schema.model.Schema;
 import lombok.extern.java.Log;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.NonNull;
 import org.eclipse.microprofile.graphql.Query;
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.AnnotationTarget;
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.DotName;
 
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.CDI;
@@ -34,9 +38,10 @@ import java.util.Map;
 import static graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION;
 import static graphql.introspection.Introspection.DirectiveLocation.INTERFACE;
 import static graphql.introspection.Introspection.DirectiveLocation.OBJECT;
+import static graphql.schema.GraphQLArgument.newArgument;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
 import static graphql.schema.GraphQLList.list;
 import static graphql.schema.GraphQLNonNull.nonNull;
-import static graphql.schema.GraphQLTypeReference.typeRef;
 import static java.util.stream.Collectors.toList;
 
 /**
@@ -45,6 +50,8 @@ import static java.util.stream.Collectors.toList;
 @Log
 @GraphQLApi
 public class Federation {
+    private static final DotName KEY = DotName.createSimple(Key.class.getName());
+
     private static final GraphQLScalarType _FieldSet = GraphQLScalarType.newScalar().name("_FieldSet")
         .coercing(new GraphqlStringCoercing()).build();
 
@@ -104,19 +111,19 @@ public class Federation {
             .build());
         // directive @key(fields: _FieldSet!) on OBJECT | INTERFACE
         builder.additionalDirective(GraphQLDirective.newDirective().name("key").validLocations(OBJECT, INTERFACE)
-            .argument(GraphQLArgument.newArgument().name("fields").type(nonNull(_FieldSet)))
+            .argument(newArgument().name("fields").type(nonNull(_FieldSet)))
             .description("The @key directive is used to indicate a combination of fields that can be used to uniquely identify " +
                 "and fetch an object or interface.")
             .build());
         // directive @provides(fields: _FieldSet!) on FIELD_DEFINITION
         builder.additionalDirective(GraphQLDirective.newDirective().name("provides").validLocations(FIELD_DEFINITION)
-            .argument(GraphQLArgument.newArgument().name("fields").type(nonNull(_FieldSet)))
+            .argument(newArgument().name("fields").type(nonNull(_FieldSet)))
             .description("The @provides directive is used to annotate the expected returned fieldset from a field on a base type " +
                 "that is guaranteed to be selectable by the gateway.")
             .build());
         // directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
         builder.additionalDirective(GraphQLDirective.newDirective().name("requires").validLocations(FIELD_DEFINITION)
-            .argument(GraphQLArgument.newArgument().name("fields").type(nonNull(_FieldSet)))
+            .argument(newArgument().name("fields").type(nonNull(_FieldSet)))
             .description("The @requires directive is used to annotate the required input fieldset from a base type for a resolver. " +
                 "It is used to develop a query plan where the required fields may not be needed by the client, " +
                 "but the service may need additional information from other services.")
@@ -129,19 +136,31 @@ public class Federation {
     }
 
     private void addUnions(Builder builder) {
+        var index = ScanningContext.getIndex();
+        var typesWithKey = index.getAnnotations(KEY).stream()
+            .map(AnnotationInstance::target)
+            .map(AnnotationTarget::asClass)
+            .map(typeInfo -> toObjectType(typeInfo, builder))
+            .toArray(GraphQLObjectType[]::new);
         // union _Entity = Content
         _Entity = GraphQLUnionType.newUnionType().name("_Entity")
-            .possibleTypes(typeRef("Content")) // TODO A: collect and register @FederatedSource parameter types
+            .possibleTypes(typesWithKey)
             .description("This is a union of all types that use the @key directive, " +
                 "including both types native to the schema and extended types.").build();
-
         builder.additionalType(_Entity);
+    }
+
+    private GraphQLObjectType toObjectType(ClassInfo typeInfo, Builder builder) {
+        var typeName = typeInfo.name().local();
+        var objectType = builder.build().getObjectType(typeName);
+        if (objectType == null) throw new IllegalStateException("no class registered in schema for " + typeName);
+        return objectType;
     }
 
     private void addQueries() {
         // _entities(representations: [_Any!]!): [_Entity]!
-        _entities = GraphQLFieldDefinition.newFieldDefinition().name("_entities")
-            .argument(GraphQLArgument.newArgument().name("representations").type(nonNull(list(nonNull(_Any)))))
+        _entities = newFieldDefinition().name("_entities")
+            .argument(newArgument().name("representations").type(nonNull(list(nonNull(_Any)))))
             .type(nonNull(list(_Entity))).build();
 
         query.field(_entities);
@@ -152,27 +171,6 @@ public class Federation {
         codeRegistry.dataFetcher(FieldCoordinates.coordinates(query.build(), _entities), this::fetchEntities);
     }
 
-
-    @Query public @NonNull _Service _service() {
-        // TODO A: derive real schema
-        return new _Service("\n" +
-            "\"Query root\"\n" +
-            "type Query {\n" +
-            "  ratings(contentId: String!): [Review!]!\n" +
-            "}\n" +
-            "\n" +
-            "type Review @key(fields: \"id\") {\n" +
-            "    id: ID!\n" +
-            "    contentId: ID!\n" +
-            "    score: String!\n" +
-            "    comments: [String!]!\n" +
-            "}\n" +
-            "\n" +
-            "type Content @key(fields: \"id\") @extends {\n" +
-            "    id: ID! @external\n" +
-            "    reviews: [Review!]!\n" +
-            "}\n");
-    }
 
     private GraphQLObjectType resolveEntity(TypeResolutionEnvironment environment) {
         var typeName = environment.getObject().getClass().getSimpleName(); // TODO B: type renames
@@ -200,24 +198,47 @@ public class Federation {
 
     /** Create a prefilled instance of the type going into the federated resolver */
     private Object toRepresentationInstance(Map<String, Object> any) {
-        var typename = (String) any.get("__typename");
+        var typeName = (String) any.get("__typename");
         try {
-            var type = schema.getTypes().get(typename);
+            var type = schema.getTypes().get(typeName);
+            if (type == null) throw new IllegalStateException("no class registered in schema for " + typeName);
             var cls = Class.forName(type.getClassName());
             Object instance = cls.getConstructor().newInstance();
             // TODO B: be smarter about renames, etc.
             for (String fieldName : type.getFields().keySet()) {
-                // TODO A: set only @external fields
                 if ("__typename".equals(fieldName)) continue;
                 var value = (String) any.get(fieldName);
                 var field = cls.getDeclaredField(fieldName);
-                field.setAccessible(true);
-                field.set(instance, value);
+                if (field.isAnnotationPresent(External.class)) {
+                    field.setAccessible(true);
+                    field.set(instance, value);
+                }
             }
             return instance;
         } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("can't create extended type instance " + typename, e);
+            throw new RuntimeException("can't create extended type instance " + typeName, e);
         }
+    }
+
+    @Query public @NonNull _Service _service() {
+        // TODO A: derive real schema
+        return new _Service("\n" +
+            "\"Query root\"\n" +
+            "type Query {\n" +
+            "  ratings(contentId: String!): [Review!]!\n" +
+            "}\n" +
+            "\n" +
+            "type Review @key(fields: \"id\") {\n" +
+            "    id: ID!\n" +
+            "    contentId: ID!\n" +
+            "    score: String!\n" +
+            "    comments: [String!]!\n" +
+            "}\n" +
+            "\n" +
+            "type Content @key(fields: \"id\") @extends {\n" +
+            "    id: ID! @external\n" +
+            "    reviews: [Review!]!\n" +
+            "}\n");
     }
 
     // TODO A: register extended type dynamically
